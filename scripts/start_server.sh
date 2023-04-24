22bb981fb45bec9ff6ae3322fd5a9947c4fdc89e
Shilpakamsani <sxk59340@ucmo.edu>
2023-04-24 12:09:34 -0500

    done

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-service httpd start
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/Finaltest1
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/Finaltest1
+
+
+#start our node app in the background
+python3 app.py > app.out.log 2> app.err.log < /dev/null &
